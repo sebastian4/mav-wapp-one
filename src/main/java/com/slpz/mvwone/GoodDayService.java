8afc60f631f78ae99e013c3b6f59b502a005fbfa
sebastian <svm4apps@gmail.com>
2014-03-10 13:06:53 -0400

    added printouts, postconstruct and predestroy

--- src/main/java/com/slpz/mvwone/GoodDayService.java
+++ src/main/java/com/slpz/mvwone/GoodDayService.java
@@ -1,5 +1,7 @@
 package com.slpz.mvwone;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -8,10 +10,21 @@ import javax.ws.rs.core.Response;
 @Path("/good")
 public class GoodDayService {
     
+//    public GoodDayService() {
+//        System.out.println("GoodDayService()");
+//    }
+    
+    @PostConstruct
+    public void initializing() {
+      System.out.println("initializing()");
+    }
+    
     @GET
     @Path("/morning/{param}")
     public Response getMorning(@PathParam("param") String msg) {
-
+            
+            System.out.println("getMorning("+msg+")");
+            
             String output = "Good Morning " + msg;
 
             return Response.status(200).entity(output).build();
@@ -21,9 +34,16 @@ public class GoodDayService {
     @Path("/afternoon/{param}")
     public Response getAfternoon(@PathParam("param") String msg) {
 
+            System.out.println("getAfternoon("+msg+")");
+            
             String output = "Good Afternoon " + msg;
 
             return Response.status(200).entity(output).build();
+    }
+    
+    @PreDestroy
+	public void cleanUp() throws Exception {
+	  System.out.println("cleanUp()");
     }
     
 }
